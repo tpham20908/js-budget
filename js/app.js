0aa971f7f6c3ab914d417371718424faba3a622b
tpham20908 <tpham20908@gmail.com>
2019-05-14 22:11:09 -0400

    added expense and calculated balance

--- js/app.js
+++ js/app.js
@@ -49,13 +49,72 @@ class UI {
 
   // total expense
   totalExpense = () => {
-    let total = 400;
+    let total = 0;
+    if (this.itemList.length > 0) {
+      total = this.itemList.reduce((acc,item) => {
+        acc += item.amount;
+        return acc;
+      }, 0);
+    }
+
+    this.expenseAmount.textContent = total;
+
     return total;
   }
 
   // submit expense method
   submitExpenseForm = () => {
-    console.log("expense form submit");
+    const expenseValue = this.expenseInput.value;
+    const amountValue = this.amountInput.value;
+    if (expenseValue === "" || amountValue === "" || amountValue < 0) {
+      this.expenseFeedback.classList.add("showItem");
+      this.expenseFeedback.innerHTML = `<p>value cannot be empty or negative</p>`;
+      // const self = this;  // only used with regular function
+      setTimeout(() => this.expenseFeedback.classList.remove("showItem"), 3000);
+    } else {
+      const amount = parseInt(amountValue);
+      // clear input fields
+      this.expenseInput.value = "";
+      this.amountInput.value = "";
+
+      // create expense object with id, title, amount
+      const expense = {
+        id: this.itemID,
+        title: expenseValue,
+        amount
+      }
+
+      // add expense obj to this.itemList
+      this.itemList.push(expense);
+
+      // display expense obj
+      this.addExpense(expense);
+
+      // show balance
+      this.showBalance();
+    }
+  }
+
+  // add expense
+  addExpense = expense => {
+    const div = document.createElement("div");
+    div.classList.add("expense");
+    div.innerHTML = `
+      <div class="expense-item d-flex justify-content-between align-items-baseline">
+        <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
+        <h5 class="expense-amount mb-0 list-item">$ ${expense.amount}</h5>
+
+        <div class="expense-icons list-item">
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+            <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${expense.id}">
+            <i class="fas fa-trash"></i>
+          </a>
+        </div>
+      </div>
+    `;
+    this.expenseList.appendChild(div);
   }
 }
 
@@ -63,7 +122,7 @@ const eventListeners = () => {
   const budgetForm = document.getElementById("budget-form");
   const expenseForm = document.getElementById("expense-form");
   const expenseList = document.getElementById("expense-list");
-  
+
   // instance of UI class
   const ui = new UI();
 
